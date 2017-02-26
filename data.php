5119bd8d33a301c613ca253c204d6c15d10efb3a
pruder <alexthebaker777@yahoo.com>
2017-02-26 15:59:31 -0600

    open shell for a bar graph, needs database integration

---
+++ data.php
@@ -0,0 +1,39 @@
+<?php
+//setting header to json
+header('Content-Type: application/json');
+
+//database
+define('DB_HOST', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Ilikepie0!');
+define('DB_NAME', 'bargraphdb');
+
+//get connection
+$mysqli = new mysqli(localhost, root, password, bargraphdb); //password removed
+
+if(!$mysqli){
+	die("Connection failed: " . $mysqli->error);
+}
+
+//query to get data from the table
+$query = sprintf("SELECT playerid, score FROM score ORDER BY playerid");
+
+//execute query
+$result = $mysqli->query($query);
+
+//loop through the returned data
+$data = array();
+foreach ($result as $row) {
+	$data[] = $row;
+}
+
+//free memory associated with result
+$result->close();
+
+//close connection
+$mysqli->close();
+
+//now print the data
+print json_encode($data);
+
+//Use localhost:3306/chartjs/data.php//
