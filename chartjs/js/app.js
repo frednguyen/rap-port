63232ac1ab87e94fd0880849f0aa29959327cc5e
pruder <alexthebaker777@yahoo.com>
2017-03-01 21:35:44 -0600

    Graphs added, needs resizing and colored bars

--- chartjs/js/app.js
+++ chartjs/js/app.js
@@ -1,29 +1,149 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	var EmotionalTone = ['Anger', 'Disgust', 'Fear', 'Joy', 'Sadness'];
-	var EmotionalPercent = ['0.187858', '0.033392', '0.077501', '0.56593', '0.222565'];
 	var LanguageTone = ['Analytical', 'Confident', 'Tentative'];
-	var LanguagePercent = ['0', '0', '0'];
-	var SocialTone = ['Openness_big5', 'Conscientiousness_big5', 'Extraversion_big5', 'Agreeableness_big5', 'Emotional_range_big5'];
+	var SocialTone = ['Openness', 'Conscientiousness', 'Extraversion', 'Agreeableness', 'Emotional Range'];
+	var EmotionalPercent = ['0.187858', '0.033392', '0.077501', '0.56593', '0.222565'];
+	var AggregateEmotionalPercent = ['0.187858', '0.033392', '0.077501', '0.56593', '0.222565'];
+	var LanguagePercent = ['0.5', '0.45', '0.05'];
+	var AggregateLanguagePercent = ['0.5', '0.45', '0.05'];
 	var SocialPercent = ['0.144215', '0.288072', '0.569773', '0.640682', '0.450624'];
+	var AggregateSocialPercent = ['0.144215', '0.288072', '0.569773', '0.640682', '0.450624'];
 
 	var emotionaldata = {
 		labels: EmotionalTone,
-		datasets : [
-		{
-			label: 'Tone percentage',
-			backgroundColor: 'rgba(200, 200, 200, 0.75)',
-			borderColor: 'rgba(200, 200, 200, 0.75)',
-			hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
-			hoverBorderColor: 'rgba(200, 200, 200, 1)',
-			data: EmotionalPercent
-		}
+		datasets: [
+			{
+				label: 'Tone',
+				backgroundColor: 'rgba(255, 0, 0, 0.75)',
+				borderColor: 'rgba(200, 200, 200, 0.75)',
+				hoverBackgroundColor: 'rgba(255, 0, 0, 1)',
+				hoverBorderColor: 'rgba(200, 200, 200, 1)',
+				data: EmotionalPercent
+			}
 		]
 	};
 
-		var ctx = $("#mycanvas");
+	var languagedata = {
+		labels: LanguageTone,
+		datasets: [
+			{
+				label: 'Tone',
+				backgroundColor: 'rgba(200, 200, 200, 0.75)',
+				borderColor: 'rgba(200, 200, 200, 0.75)',
+				hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+				hoverBorderColor: 'rgba(200, 200, 200, 1)',
+				data: LanguagePercent
+			}
+		]
+	};
 
-		var barGraph = new Chart(ctx, {
-			type: 'horizontalBar',
-			data: emotionaldata
-		});
+	var socialdata = {
+		labels: SocialTone,
+		datasets: [
+			{
+				label: 'Tone',
+				backgroundColor: 'rgba(200, 200, 200, 0.75)',
+				borderColor: 'rgba(200, 200, 200, 0.75)',
+				hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+				hoverBorderColor: 'rgba(200, 200, 200, 1)',
+				data: SocialPercent
+			}
+		]
+	};
+
+	var aggregateemotionaldata = {
+		labels: EmotionalTone,
+		datasets: [
+			{
+				label: 'Tone',
+				backgroundColor: 'rgba(255, 0, 0, 0.75)',
+				borderColor: 'rgba(200, 200, 200, 0.75)',
+				hoverBackgroundColor: 'rgba(255, 0, 0, 1)',
+				hoverBorderColor: 'rgba(200, 200, 200, 1)',
+				data: AggregateEmotionalPercent
+			}
+		]
+	};
+
+	var aggregatelanguagedata = {
+		labels: LanguageTone,
+		datasets: [
+			{
+				label: 'Tone',
+				backgroundColor: 'rgba(200, 200, 200, 0.75)',
+				borderColor: 'rgba(200, 200, 200, 0.75)',
+				hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+				hoverBorderColor: 'rgba(200, 200, 200, 1)',
+				data: AggregateLanguagePercent
+			}
+		]
+	};
+
+	var aggregatesocialdata = {
+		labels: SocialTone,
+		datasets: [
+			{
+				label: 'Tone',
+				backgroundColor: 'rgba(200, 200, 200, 0.75)',
+				borderColor: 'rgba(200, 200, 200, 0.75)',
+				hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+				hoverBorderColor: 'rgba(200, 200, 200, 1)',
+				data: AggregateSocialPercent
+			}
+		]
+	};
+
+	var c1 = $("#canvasone");
+
+	var barGraph = new Chart(c1, {
+		type: 'horizontalBar',
+		responsive: true,
+		maintainAspectRatio: false,
+		data: emotionaldata
+	})
+
+	var c2 = $("#canvastwo");
+
+	var barGraph = new Chart(c2, {
+		type: 'horizontalBar',
+		responsive: true,
+		maintainAspectRatio: false,
+		data: languagedata
+	})
+
+	var c3 = $("#canvasthree");
+
+	var barGraph = new Chart(c3, {
+		type: 'horizontalBar',
+		responsive: true,
+		maintainAspectRatio: false,
+		data: socialdata
+	})
+
+	var c4 = $("#canvasfour");
+
+	var barGraph = new Chart(c4, {
+		type: 'horizontalBar',
+		responsive: true,
+		maintainAspectRatio: false,
+		data: aggregateemotionaldata
+	})
+
+	var c5 = $("#canvasfive");
+
+	var barGraph = new Chart(c5, {
+		type: 'horizontalBar',
+		responsive: true,
+		maintainAspectRatio: false,
+		data: aggregatelanguagedata
+	})
+
+	var c6 = $("#canvassix");
+
+	var barGraph = new Chart(c6, {
+		type: 'horizontalBar',
+		responsive: true,
+		maintainAspectRatio: false,
+		data: aggregatesocialdata
+	});
 });
