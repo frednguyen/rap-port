6e70e93c8f5637dc03a143f85b739ae9d42b49c2
Aldo Sanchez <Aldo Sanchez>
2017-03-03 21:34:53 -0600

    :construction: Here we go. First attempt to deploy

---
+++ config/watson_keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  username: process.env.watsonUsername,
+  password: process.env.watsonPassword,
+  version: process.env.watsonVersion,
+  version_date: process.env.watsonVersion_date
+};
